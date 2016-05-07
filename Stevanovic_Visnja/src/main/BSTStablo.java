e57f874811fe2cf9625f7298fd4c4c7316da3c21
visnjastevanovic714 <visnja.stevanovic5@gmail.com>
2016-05-07 11:52:07 +0200

    Implementirana metoda koja ispisuje rastuce u klasi BSTStablo.

--- Stevanovic_Visnja/src/main/BSTStablo.java
+++ Stevanovic_Visnja/src/main/BSTStablo.java
@@ -1,7 +1,17 @@
 package main;
 
+import labis.cvorovi.CvorStabla;
+import labis.exception.LabisException;
 import labis.stabla.ABSTStablo;
 
-public class BSTStablo extends ABSTStablo{
-	
+public class BSTStablo extends ABSTStablo {
+
+	@Override
+	public void ispisiRastuce(CvorStabla k) throws LabisException {
+		if (k == null)
+			return;
+		ispisiRastuce(k.levo);
+		System.out.println(k.podatak + "");
+		ispisiRastuce(k.desno);
+	}
 }
